1264b947bc47b8323a77e4db805126b3ca49874b
mahdi <mahdimotallebe@gmail.com>
2022-11-30 13:49:39 +0330

    Implement footer

---
+++ src/pages/global/footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Mui
+import { Box } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Box textTransform="capitalize" p={3}>
+        all right reserved.
+      </Box>
+    </footer>
+  );
+};
+
+export default Footer;
